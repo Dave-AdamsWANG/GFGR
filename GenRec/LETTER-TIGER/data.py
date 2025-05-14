d261d0bfbef97121a401f2b906f9a194187340c7
root <yejing.wang@my.cityu.edu.hk>
2025-05-14 13:46:05 +0800

    sdpo multiple negatives

--- GenRec/LETTER-TIGER/data.py
+++ GenRec/LETTER-TIGER/data.py
@@ -404,6 +404,13 @@ class SeqRecDataset(BaseDataset):
         if hasattr(self,'rl_type'):
             if self.rl_type in ['dpo','ipa','sprec','sdpo']:
                 return dict(prompt=d["inters"], chosen=d["item"],rejected=d["neg"][0],origin_item=d["origin_item"],origin_inters=d["origin_inters"],positions=d["positions"])
+            elif self.rl_type=='sdpo':
+                res = dict(prompt=d["inters"], chosen=d["item"],rejected=[0],origin_item=d["origin_item"],origin_inters=d["origin_inters"],positions=d["positions"])
+                cnt = 0  
+                for rejected in d["neg"]:
+                    cnt += 1
+                    res[f"rejected{cnt}"]=rejected
+                return res
             elif self.rl_type in ['grpo','ppo']:
                 return dict(prompt=d["inters"], ground_truth=d["item"],origin_item=d["origin_item"],origin_inters=d["origin_inters"],positions=d["positions"])
         else:
