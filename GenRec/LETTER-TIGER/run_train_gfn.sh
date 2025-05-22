72866f526bb1533881b8a4c5b186d84ef9618c74
root <yejing.wang@my.cityu.edu.hk>
2025-05-22 19:45:40 +0800

    update script

--- GenRec/LETTER-TIGER/run_train_gfn.sh
+++ GenRec/LETTER-TIGER/run_train_gfn.sh
@@ -8,30 +8,58 @@ DATA_PATH=/root/LETTER/data
 # weight=0.001
 # OUTPUT_DIR=/root/autodl-tmp/ckpt/$DATASET/gfn/${neg_num}/${weight}
 # seed_list=(43 44)
-<<<<<<< HEAD
-    # --collab_reward \    --pretrained --reward_m \
-=======
-    # --collab_reward \    --pretrained
->>>>>>> refs/remotes/origin/new-main
 
+    # --collab_reward \    --pretrained --reward_m \
+
+    # --collab_reward \    --pretrained
 # .index.epoch10000.alpha0-beta0.json "Beauty"
 # neg_num_list=(1 3 5)
 # weight_list=(1.0 0.1 0.01 0.001)
-<<<<<<< HEAD
-neg_num_list=(1)
-=======
-neg_num_list=(5)
->>>>>>> refs/remotes/origin/new-main
-weight_list=(0.02)
-data_list=("Beauty")
+# bash run.sh
+neg_num_list=(3)
+weight_list=(0.1 10.0 100.0)
+data_list=("Instruments")
 for DATASET in ${data_list[@]}
 do
 ORG_CKPT_DIR=/root/autodl-tmp/org_chkpt_TIGER/ckpt/$DATASET/
-RESULTS_FILE=/root/autodl-tmp/results/$DATASET/TIGER/
 for neg_num in ${neg_num_list[@]}
 do
     for weight in ${weight_list[@]}
     do 
+    # RESULTS_FILE=/root/autodl-tmp/results/$DATASET/TIGER/
+    # OUTPUT_DIR=/root/autodl-tmp/ckpt/$DATASET/TIGER/gfn/${neg_num}/${weight}
+    # python ./finetune_gfn.py \
+    # --output_dir $OUTPUT_DIR \
+    # --dataset $DATASET \
+    # --data_path $DATA_PATH \
+    # --gfn_ckpt_path $ORG_CKPT_DIR \
+    # --per_device_batch_size 256 \
+    # --learning_rate 5e-4 \
+    # --epochs 200 \
+    # --index_file .new.index.json \
+    # --temperature 1.0 \
+    # --gfn_neg_num ${neg_num} \
+    # --gfn_weight ${weight}  \
+    # --collab_model_path /root/GFGR/SeqRec/saved/$DATASET/bert4rec/pytorch_model.bin \
+    # --collab_reward \
+    # --token_reward \
+    # --reward_weigted_loss \
+    # --pretrained
+    
+    # python test.py \
+    #     --gpu_id 0 \
+    #     --ckpt_path $OUTPUT_DIR \
+    #     --dataset $DATASET \
+    #     --data_path $DATA_PATH \
+    #     --results_file $RESULTS_FILE \
+    #     --test_batch_size 480 \
+    #     --num_beams 20 \
+    #     --test_prompt_ids 0 \
+    #     --index_file .new.index.json \
+    #     --gfn_neg_num ${neg_num} \
+    #     --gfn_weight ${weight} 
+
+    RESULTS_FILE=/root/autodl-tmp/results/$DATASET/
     OUTPUT_DIR=/root/autodl-tmp/ckpt/$DATASET/gfn/${neg_num}/${weight}
     python ./finetune_gfn.py \
     --output_dir $OUTPUT_DIR \
@@ -41,28 +69,16 @@ do
     --per_device_batch_size 256 \
     --learning_rate 5e-4 \
     --epochs 200 \
-    --index_file .new.index.json \
+    --index_file .index.json \
     --temperature 1.0 \
     --gfn_neg_num ${neg_num} \
     --gfn_weight ${weight}  \
     --collab_model_path /root/GFGR/SeqRec/saved/$DATASET/bert4rec/pytorch_model.bin \
     --collab_reward \
     --token_reward \
-<<<<<<< HEAD
-=======
-    --reward_m \
->>>>>>> refs/remotes/origin/new-main
-    --reward_label_align \
-    --collab_align \
     --reward_weigted_loss \
-    --pretrained \
-<<<<<<< HEAD
-    --align_weight 1.0 \
-=======
-    --align_weight 0.01 \
->>>>>>> refs/remotes/origin/new-main
-
-
+    --pretrained
+    
     python test.py \
         --gpu_id 0 \
         --ckpt_path $OUTPUT_DIR \
@@ -75,7 +91,7 @@ do
         --index_file .new.index.json \
         --gfn_neg_num ${neg_num} \
         --gfn_weight ${weight} 
-        
+
 
     done
 done
